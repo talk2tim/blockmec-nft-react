2da88e7f1d66664828d331796f66a3c18f92b980
talk2tim <timothyazubuikeh@gmail.com>
2022-03-15 19:21:37 -0400

    finished  up and css

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Header from './components/Header'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Punklist from './components/Punklist';
+import Main from './components/Main';
 
 function App() {
 
-  const[punkListData, setPunkListData] = useState([])
+  const [punkListData, setPunkListData] = useState([])
+  const [selectedPunk, setSelectedPunk] = useState(0)
 
   useEffect(() => {
     const getMyNfts = async () => {
@@ -26,8 +28,16 @@ function App() {
   return (
     <div className="app">
       <Header />
-
-      <Punklist punkListData={punkListData} /> 
+      {punkListData.length > 0 && (
+        <>
+          <Main punkListData={punkListData} selectedPunk={selectedPunk}/>
+          <Punklist 
+            punkListData={punkListData} 
+            setSelectedPunk={setSelectedPunk} 
+          /> 
+        </>
+        )}
+      
     </div> 
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import instagramLogo from '../assets/owner/instagram.png'
+import twitterLogo from '../assets/owner/twitter.png'
+import moreIcon from '../assets/owner/more.png'
+import './Main.css'
 
-const Main = () => {
+const Main = ({ selectedPunk, punkListData }) => {
+
+    const [activePunk, setActivePunk] = useState(punkListData[0])
+    // src='https://ipfs.thirdweb.com/ipfs/QmXt9PsvwBQvs4kHbbM1Gt3aPcZTrFfQNcY6eRWVW6d3aH/0.jpg'
+
+    // They are using image_original_url. i am using image_preview_url
+
+    useEffect(() => {
+        setActivePunk(punkListData[selectedPunk])
+    }, [punkListData, selectedPunk])
   return (
-    <div>Main</div>
+    <div className="main">
+        <div className="mainContent">
+            <div className="punkHighlight">
+                <div className="punkContainer">
+                    <img 
+                        className="selectedPunk" 
+                        src={activePunk.image_preview_url} 
+                        alt="activePunk" 
+                    />
+                </div>
+            </div>
+
+            <div className="punkDetails" style={{ color: 'white' }}>
+              <div className="title">
+                {activePunk.name}
+              
+              <span className="itemNumber"> #{activePunk.token_id}</span>
+           </div>
+
+        <div className="owner">
+            <div className="ownerImageContainer">
+                <img 
+                    src={activePunk.owner.profile_img_url} 
+                    alt="ownerImage" className="ownerImage" 
+                />
+            </div>
+
+            <div className="ownerDetails">
+                <div className="ownerNameAndHandle">
+                    <div className="ownerAddress">{activePunk.owner.address}</div>
+                    <div className="ownerHandle">@kingslayerv2.1</div>
+                </div>
+
+                <div className="ownerLink">
+                    <img src={instagramLogo} alt="instagram" className="instagram" />
+                </div>
+
+                <div className="ownerLink">
+                    <img src={twitterLogo} alt="instagram" className="twitter" />
+                </div>
+
+                <div className="ownerLink">
+                    <img src={moreIcon} alt="moreIcon" className="moreIcon" />
+                </div>
+            </div>
+            </div>
+
+
+        </div>
+    </div>
+    </div>
   )
 }
 
